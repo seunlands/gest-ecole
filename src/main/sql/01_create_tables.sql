6878c835f821cb3c7c35793f2157cdd2d013919e
landocore <seun.landsberg@gmail.com>
2016-04-20 07:34:32 +0200

    add sql create tables

---
+++ src/main/sql/01_create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+   `username` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+   `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+   `role` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+   `enabled` tinyint(1) NOT NULL,
+   `id_famille` int(11) DEFAULT NULL,
+   PRIMARY KEY (`username`),
+   UNIQUE KEY `username_UNIQUE` (`username`),
+   KEY `fk_user_famille` (`id_famille`),
+   CONSTRAINT `fk_user_famille` FOREIGN KEY (`id_famille`) REFERENCES `familles` (`IDfamille`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
