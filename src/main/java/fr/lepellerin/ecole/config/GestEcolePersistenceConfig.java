1eb33051da346fe82c99f72fb6793af07ecff8d1
landocore <seun.landsberg@gmail.com>
2016-05-10 13:10:14 +0200

    externalisation de la config de la BDD

--- src/main/java/fr/lepellerin/ecole/config/GestEcolePersistenceConfig.java
+++ src/main/java/fr/lepellerin/ecole/config/GestEcolePersistenceConfig.java
@@ -1,55 +1,41 @@
 package fr.lepellerin.ecole.config;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.Properties;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories("fr.lepellerin.ecole.repo")
 public class GestEcolePersistenceConfig {
 
-  /**
-   * 
-   * @return retourne l'entity manager.
-   */
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource(dataSource());
-    em.setPackagesToScan(new String[] { "fr.lepellerin.ecole.bean" });
-    em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-    em.setJpaProperties(additionalProperties());
-    return em;
-  }
+	  /**
+	   * 
+	   * @return retourne l'entity manager.
+	   */
+	  @Bean
+	  @Autowired
+	  public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource datasource){
+	    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+	    em.setDataSource(datasource);
+	    em.setPackagesToScan(new String[] { "fr.lepellerin.ecole.bean" });
+	    em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+	    em.setJpaProperties(additionalProperties());
+	    return em;
+	  }
 
-  /**
-   * init data source.
-   * 
-   * @return la data source
-   */
-  @Bean
-  public DataSource dataSource() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-    dataSource
-        .setUrl("jdbc:mysql://localhost:3306/essai_cantine_data?autoReconnect=true&useSSL=false");
-    dataSource.setUsername("root");
-    dataSource.setPassword("cha04ise+");
-    return dataSource;
-  }
 
   /**
    * init transaction manager.
---
+++ src/test/java/fr/lepellerin/ecole/security/GeneratePasswords.java
@@ -0,0 +1,18 @@
+package fr.lepellerin.ecole.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class GeneratePasswords {
+
+  public static void main(String[] args) {
+
+    String password = "admin";
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(11);
+    String hashedPassword = passwordEncoder.encode(password);
+
+    System.out.println(hashedPassword);
+
+  }
+
+}
