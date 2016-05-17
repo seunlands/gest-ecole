aadf9f67a79fa2b43dc60b38e15b25f714e11718
landocore <seun.landsberg@gmail.com>
2016-05-17 07:03:07 +0200

    forgotten pwd

--- src/main/java/fr/lepellerin/ecole/web/controller/LoginController.java
+++ src/main/java/fr/lepellerin/ecole/web/controller/LoginController.java
@@ -49,7 +49,7 @@ public class LoginController {
     if (auth != null) {
       new SecurityContextLogoutHandler().logout(request, response, auth);
     }
-    return "redirect:/login?logout";
+    return "redirect:/login?error";
   }
 
   /**
