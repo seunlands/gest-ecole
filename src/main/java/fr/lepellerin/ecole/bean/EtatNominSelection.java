8d997a3b7b16caae43c41eb8364707a8d2121dd1
landocore <seun.landsberg@gmail.com>
2016-05-23 13:21:43 +0200

    correction sonar

--- src/main/java/fr/lepellerin/ecole/bean/EtatNominSelection.java
+++ src/main/java/fr/lepellerin/ecole/bean/EtatNominSelection.java
@@ -22,6 +22,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -45,7 +46,7 @@ public class EtatNominSelection implements Serializable {
   private String code;
 
   @ManyToOne
-  @Column(name = "IDprofil")
+  @JoinColumn(name = "IDprofil")
   private EtatNominProfil profil;
 
   private int ordre;
