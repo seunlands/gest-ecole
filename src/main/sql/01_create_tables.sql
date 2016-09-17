6ddda391cbe3f8c80f31cbc2d3e16d00f62ec4ac
seun landsberg <slands@protonmail.ch>
2016-09-17 15:07:57 +0200

    maj sql ajout table parametre_web

--- src/main/sql/01_create_tables.sql
+++ src/main/sql/01_create_tables.sql
@@ -8,4 +8,14 @@ CREATE TABLE `users` (
    UNIQUE KEY `username_UNIQUE` (`username`),
    KEY `fk_user_famille` (`id_famille`),
    CONSTRAINT `fk_user_famille` FOREIGN KEY (`id_famille`) REFERENCES `familles` (`IDfamille`)
- ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ 
+CREATE TABLE `parametres_web` (
+   `id_parametre` int(11) NOT NULL,
+   `libelle_parametre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+   `valeur_parametre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+   PRIMARY KEY (`id_parametre`),
+   UNIQUE KEY `id_parametre_UNIQUE` (`id_parametre`),
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ 
+ 
