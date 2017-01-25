86c519f995a6e0963f748ffd90eb5d0b3e21bbfa
Seun Landsberg <seun.landsberg@gmail.com>
2017-01-25 13:30:59 +0100

    Update LoginController.java

--- src/main/java/fr/lepellerin/ecole/web/controller/LoginController.java
+++ src/main/java/fr/lepellerin/ecole/web/controller/LoginController.java
@@ -25,6 +25,7 @@ import fr.lepellerin.ecole.service.dto.ForgottenPwdDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
